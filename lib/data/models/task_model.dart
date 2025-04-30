8d68ecdd4eea1a26cbfaa806634c790d64371693
Rubayat Islam <128803957+rubayatkhusbu25@users.noreply.github.com>
2025-04-30 21:45:26 +0600

    Update task_model.dart

--- lib/data/models/task_model.dart
+++ lib/data/models/task_model.dart
@@ -14,7 +14,7 @@ class TaskModel {
   late final String createdDate;
 
   TaskModel.fromJson(Map<String, dynamic>jsonData){
-    id =jsonData['id'];
+    id =jsonData['_id']?? "";
     title = jsonData["title"]?? "";
     description = jsonData['description']?? "";
     status = jsonData['status'];
